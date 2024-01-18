829554d77946beb7bfb03a2d8e95e3c2c33894af
Vít Rozsíval <vit.rozsival@apitree.cz>
2024-01-18 14:57:06 +0100

    fix(eslint-config): allow .spec.tsx tests

--- packages/eslint-config/src/react/overrides/tests.ts
+++ packages/eslint-config/src/react/overrides/tests.ts
@@ -5,12 +5,12 @@ import { tests as base } from '../../base/overrides/tests';
 export const tests: Linter.ConfigOverride[] = [
   { ...base, files: [...base.files, '*.spec.tsx', '*.test.tsx'] },
   {
-    files: '*.test.tsx',
+    files: ['*.spec.tsx', '*.test.tsx'],
     extends: ['plugin:jest-dom/recommended', 'plugin:testing-library/react'],
     rules: base.rules,
   },
   {
-    files: ['*.stories.tsx', '*.test.tsx'],
+    files: ['*.spec.tsx', '*.stories.tsx', '*.test.tsx'],
     rules: {
       'formatjs/no-literal-string-in-jsx': 'off', // Allow literal strings in JSX in stories and tests
       'react/jsx-props-no-spreading': 'off', // Allow spreading props in stories and tests
