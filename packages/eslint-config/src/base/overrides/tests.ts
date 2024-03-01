c634d27eb584f7da4cadedb6bd7602f59444a7c8
Vít Rozsíval <vit.rozsival@apitree.cz>
2024-03-01 21:07:14 +0100

    fix(eslint-config): fix path overrides

--- packages/eslint-config/src/base/overrides/tests.ts
+++ packages/eslint-config/src/base/overrides/tests.ts
@@ -1,7 +1,7 @@
 import type { Linter } from 'eslint';
 
 export const tests = {
-  files: ['*.spec.ts', '*.test.ts', '/mock/**/*', '/mocks/**/*', '/tests/**/*'],
+  files: ['*.spec.ts', '*.test.ts', 'mock/**/*', 'mocks/**/*', 'tests/**/*'],
   extends: ['plugin:vitest/recommended'],
   rules: {
     'no-magic-numbers': 'off', // Allow magic numbers in tests and mocks
