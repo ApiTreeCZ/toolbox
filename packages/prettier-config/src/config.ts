cbb1d582f16968492ff636173ca3c5970a978f1e
Ondřej Langr <ondrej.langr@apitree.cz>
2024-03-06 14:37:27 +0100

    feat: adds prettier sort plugin to configuration and configures it

--- packages/prettier-config/src/config.ts
+++ packages/prettier-config/src/config.ts
@@ -3,7 +3,12 @@ import type { Config } from 'prettier';
 export const config = {
   arrowParens: 'always',
   bracketSpacing: true,
-  plugins: ['prettier-plugin-packagejson'],
+  plugins: ['prettier-plugin-packagejson', '@trivago/prettier-plugin-sort-imports'],
+  // Group by external libraries and local files
+  // TODO: load local import prefixes from tsconfig?
+  importOrder: ['^node:(.*)', '<THIRD_PARTY_MODULES>', '^[./]'],
+  // Separate import groups by newline
+  importOrderSeparation: true,
   printWidth: 120,
   semi: true,
   singleQuote: true,
