f5163a3e033d4a3ef6e5350e407407e45126a95d
Vít Rozsíval <vit.rozsival@apitree.cz>
2024-03-19 10:55:42 +0100

    Revert "feat: removes redundant import/order eslint rules"
    This reverts commit 23317bf737118eee170b7a3b901ae3a079cac52d.

--- packages/eslint-config/src/base/rules/imports.ts
+++ packages/eslint-config/src/base/rules/imports.ts
@@ -5,6 +5,18 @@ import { getExtraneousDependenciesRule, getImportExtensionsRule } from '../../ut
 export const imports: Linter.RulesRecord = {
   ...getImportExtensionsRule(),
   ...getExtraneousDependenciesRule('/mock/**/*', '/mocks/**/*'), // Allow importing devDependencies in mock files
+  'import/order': [
+    // Sort and group imports by type
+    'error',
+    {
+      alphabetize: {
+        caseInsensitive: true,
+        order: 'asc',
+      },
+      groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+      'newlines-between': 'always',
+    },
+  ],
   'import/prefer-default-export': 'off', // Prefer named exports
   'unused-imports/no-unused-imports': 'error', // Disallow unused imports
   'unused-imports/no-unused-vars': ['error', { ignoreRestSiblings: true }], // Disallow unused variables
