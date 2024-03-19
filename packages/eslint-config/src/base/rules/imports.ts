5c69480c68e3dd9fd7208efd60950827dafd2fc9
Vít Rozsíval <vit.rozsival@apitree.cz>
2024-03-19 10:55:42 +0100

    Revert "feat: disables import/order rule altogether"
    This reverts commit c8d1dd6e1a0053fba5cea1b2688a0d9c7b42a530.

--- packages/eslint-config/src/base/rules/imports.ts
+++ packages/eslint-config/src/base/rules/imports.ts
@@ -8,5 +8,4 @@ export const imports: Linter.RulesRecord = {
   'import/prefer-default-export': 'off', // Prefer named exports
   'unused-imports/no-unused-imports': 'error', // Disallow unused imports
   'unused-imports/no-unused-vars': ['error', { ignoreRestSiblings: true }], // Disallow unused variables
-  'import/order': 'off',
 };
