8854d243a26636e416643111966ef8cff36bb024
Vít Rozsíval <vit.rozsival@apitree.cz>
2025-01-29 01:49:11 +0100

    feat(eslint-config): allow bool, number and nullish template expression

--- packages/eslint-config/src/base/rules/typescript.ts
+++ packages/eslint-config/src/base/rules/typescript.ts
@@ -9,5 +9,10 @@ export const typescript: Linter.RulesRecord = {
   '@typescript-eslint/no-unsafe-declaration-merging': 'error', // Guard declaration merging against unsafe usage
   '@typescript-eslint/no-unused-vars': 'off', // Guarded by eslint-plugin-unused-imports
   '@typescript-eslint/no-use-before-define': 'error', // Disallow usage of variables before their declaration
+  // Enforce template literal expressions to be of string type
+  '@typescript-eslint/restrict-template-expressions': [
+    'error',
+    { allowBoolean: true, allowNumber: true, allowNullish: true },
+  ],
   '@typescript-eslint/sort-type-constituents': 'error', // Sort union or intersection type members alphabetically
 };
