d56334f9f27c5fe383d6375f78a1b452a3fb0dff
Vít Rozsíval <vit.rozsival@apitree.cz>
2025-01-30 19:57:10 +0100

    chore(lint-staged): fix github pattern

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,5 +2,5 @@ export default {
   './*.{cjs,js}': 'eslint --cache --fix',
   './*.{json,md,yml,yaml}': 'prettier --write',
   './.changeset/**/*.{json,md}': 'prettier --write',
-  './github/**/*.{yml,yaml}': 'prettier --write',
+  './.github/**/*.{yml,yaml}': 'prettier --write',
 };
