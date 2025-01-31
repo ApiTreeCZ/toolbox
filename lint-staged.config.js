61221f9192c5d775c9ac7a8694d567d910a2cba8
Vít Rozsíval <vit.rozsival@apitree.cz>
2025-01-31 01:48:34 +0100

    chore(lint-staged): sort targets [ci skip]

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
 export default {
-  './*.js': 'eslint --cache --fix',
-  './*.{js,json,md,yml,yaml}': 'prettier --write',
   './.changeset/**/*.{json,md}': 'prettier --write',
   './.github/**/*.{yml,yaml}': 'prettier --write',
+  './*.js': 'eslint --cache --fix',
+  './*.{js,json,md,yml,yaml}': 'prettier --write',
 };
