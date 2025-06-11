5f1d3883bf245b374480977bdacc00ce19121392
Vít Rozsíval <vit.rozsival@apitree.cz>
2025-06-11 21:03:19 +0200

    fix(eslint-config): enforcer rules of hooks (closes #70)

---
+++ .changeset/ready-lights-watch.md
@@ -0,0 +1,5 @@
+---
+'@apitree.cz/eslint-config': patch
+---
+
+Enforce Rules of Hooks (closes [#70](https://github.com/ApiTreeCZ/toolbox/issues/70)).
--- packages/eslint-config/src/react/rules/hooks.ts
+++ packages/eslint-config/src/react/rules/hooks.ts
@@ -2,4 +2,5 @@ import type { Linter } from 'eslint';
 
 export const hooks: Linter.RulesRecord = {
   'react-hooks/exhaustive-deps': 'error', // Disallow missing dependencies in React hooks
+  'react-hooks/rules-of-hooks': 'error', // Enforce rules of hooks
 };
